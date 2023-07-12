0438fd234882bffb380324fd23042d78c3265563
esirbu <evghenii.sirbu@yahoo.com>
2023-07-13 02:18:22 +0300

    added login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,251 @@
+'use client'
+import React,{useState} from 'react'
+// import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+    // const navigate = useNavigate();
+    const [isLogin,setIsLogin] = useState(true);
+  
+    const [loginEmail,setLoginEmail] = useState("");
+    const [loginPassword,setLoginPassword] = useState("");
+  
+    const [registerEmail,setRegisterEmail] = useState("");
+    const [registerPassword,setRegisterPassword] = useState("");
+    const [registerName,setRegisterName] = useState("");
+    // const [registerAvatar,setRegisterAvatar] = useState("");
+
+const LoginForm = () => {
+    return(
+<section className="">
+
+    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+          Sign in to your account
+        </h1>
+        <form className="space-y-4 md:space-y-6" action="#">
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="name@company.com"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <div className="flex items-start">
+              <div className="flex items-center h-5">
+                <input
+                  id="remember"
+                  aria-describedby="remember"
+                  type="checkbox"
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                  required
+                />
+              </div>
+              <div className="ml-3 text-sm">
+                <label
+                  htmlFor="remember"
+                  className="text-gray-500 dark:text-gray-300"
+                >
+                  Remember me
+                </label>
+              </div>
+            </div>
+            <a
+              href="#"
+              className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+            >
+              Forgot password?
+            </a>
+          </div>
+          <button
+            type="submit"
+            className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
+            Sign in
+          </button>
+          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+            Don’t have an account yet?{" "}
+            <a
+              onClick={() => setIsLogin(false)}
+              href="#"
+              className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+            >
+              Sign up
+            </a>
+          </p>
+        </form>
+      </div>
+    </div>
+</section>
+
+    )
+  }
+  
+  const  SignUpForm = () => {
+     return(
+
+<section className="">
+  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+          Register your account
+        </h1>
+        <form className="space-y-4 md:space-y-6" action="#">
+
+        <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="John"
+              required
+              onChange={(e) => setRegisterName(e.target.value)}
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="name@mail.com"
+              required
+              onChange={(e) => setRegisterEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="••••••••"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="confirm password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Confirm password
+            </label>
+            <input
+              type="password"
+              name="Confirm password"
+              id="confirm password"
+              placeholder="••••••••"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
+          </div>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-start">
+              <div className="flex items-center h-5">
+                <input
+                  id="remember"
+                  aria-describedby="remember"
+                  type="checkbox"
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                  required
+                />
+              </div>
+              <div className="ml-3 text-sm">
+                <label
+                  htmlFor="remember"
+                  className="text-gray-500 dark:text-gray-300"
+                >
+                  Remember me
+                </label>
+              </div>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="w-full border text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
+            Sign in
+          </button>
+          <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+            Already have an account?{" "}
+            <a
+              onClick={() => setIsLogin(true)}
+              href="#"
+              className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+            >
+              Sign in
+            </a>
+          </p>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+     )
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen md:py-2 bg-gradient-radial from-gray-600 to-gray-900">
+    <main className="flex px-2 md:px-20 items-center">
+      {
+        isLogin? (
+         <LoginForm/>
+        ):(
+         <SignUpForm/>
+        )
+      }
+    </main>
+    </div>
+  )
+}
+
+export default Login
